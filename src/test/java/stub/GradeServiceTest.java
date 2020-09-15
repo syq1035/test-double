93c20523704968d9a62396a64d1477ede3428329
syq <1035394794@qq.com>
2020-09-15 23:14:46 +0800

    [syq] feat: implement grade system stub

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,35 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+    @Mock
+    private GradeSystem mockGradeSystem;
+
+    @InjectMocks
+    private GradeService gradeService;
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //        Assertions.assertEquals(90.0, result);
+        long studentId = 1;
+        when(mockGradeSystem.gradesFor(studentId))
+                .thenReturn(Arrays.asList(80.0, 90.0, 100.0));
+        double result =  gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
     }
 }
